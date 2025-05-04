5506c24fabae38aafbf5babc2408e153589f6f93
s1tkeyz <timstudiocode@gmail.com>
2025-05-04 22:39:47 +0300

    добавил исключения

---
+++ metadata_writer/src/core/exceptions.py
@@ -0,0 +1,15 @@
+class DomainException(Exception):
+    def __init__(self, message: str, status_code: int = 400):
+        self.message = message
+        self.status_code = status_code
+
+    def __str__(self):
+        return self.message
+
+class DatabaseException(DomainException):
+    def __init__(self, message: str = "Ошибка в работе базы данных"):
+        super().__init__(message, status_code=500)
+
+class NoSuchAlbumException(DomainException):
+    def __init__(self, message: str = "Запрашиваемого альбома не существует"):
+        super().__init__(message, status_code=404)
