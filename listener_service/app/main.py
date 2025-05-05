43ec23f5c9a11fe30971c5d9abf240ad5cacd128
lexasy <lexav2016@mail.ru>
2025-05-06 02:47:05 +0300

    Докинул абстракций под исключения для бд

--- listener_service/app/main.py
+++ listener_service/app/main.py
@@ -1,8 +1,5 @@
-import uvicorn
-import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Depends
-from datetime import date
 
 from infra.database.models import start_mapping
 from dependencies.main import setup_dependencies
@@ -11,16 +8,11 @@ from infra.database.repository.abc.listener import BaseListenerRepo
 from domain.entities.real.listener import Listener
 from domain.values.real.age import Age
 from domain.values.real.name import Name
-from domain.exceptions.real.names import (
-    NameTooLongException,
-    NotRealNameException,
-)
-from domain.exceptions.real.age import (
-    AgeTooBigException,
-    AgeTooSmallException,
-    AgeIncorrectFormat,
-)
 from domain.exceptions.abc.base import AplicationException
+from infra.database.exceptions.abc.base import (
+    DatabaseException,
+    DatabaseErrorException
+)
 
 
 @asynccontextmanager
@@ -41,6 +33,7 @@ for interfece, _ in deps.items():
     logger.info(f"DEPENDENCY: {interfece}")
 logger.info("Dependencies set up!")
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello world!"}
@@ -54,17 +47,25 @@ async def create_listener(
     ):
     try:
         new_listener = Listener.add_listener(Name(first_name), Name(last_name), Age(birth_date))
-        listener = await listener_repository.insert_listener(listener=new_listener) # дописать проверку на уникальность
+        listener = await listener_repository.insert_listener(listener=new_listener)
         return {"id": listener.oid, "first_name": listener.firstname, "last_name": listener.lastname}
     except AplicationException as e:
         raise HTTPException(status_code=422, detail=e.message)
+    except DatabaseException as e:
+        raise HTTPException(status_code=423, detail=e.message)
+    except DatabaseErrorException as e:
+        raise HTTPException(status_code=500, detail=e.message)
 
 @app.get("/listeners/{listener_id}")
 async def read_listener(
         listener_id: int,
         listener_repository: BaseListenerRepo = Depends()
     ):
-    listener = await listener_repository.get_listener(listener_id=listener_id)
-    if listener:
-        return {"id": listener.oid, "first_name": listener.firstname, "last_name": listener.lastname}
-    raise HTTPException(status_code=404, detail="Listener not found")
+    try:
+        listener = await listener_repository.get_listener(listener_id=listener_id)
+        if listener:
+            return {"id": listener.oid, "first_name": listener.firstname, "last_name": listener.lastname}
+    except DatabaseException as e:
+        raise HTTPException(status_code=423, detail=e.message)
+    except DatabaseErrorException as e:
+        raise HTTPException(status_code=500, detail=e.message)
