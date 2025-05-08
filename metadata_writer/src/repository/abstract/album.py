501dc2ade2e8e290933c5ae655388d6c74d45380
s1tkeyz <timstudiocode@gmail.com>
2025-05-08 11:37:54 +0300

    реструктуризация

--- metadata_writer/src/repository/abstract/album.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from src.models.album import Album
-
-class AlbumRepositoryABC(ABC):
-    @abstractmethod
-    async def create_album(self, album: Album) -> Album:
-        ...
--- metadata_writer/src/repository/abstract/track.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from src.models.track import Track
-
-class TrackRepositoryABC(ABC):
-    @abstractmethod
-    async def create_track(self, track: Track) -> Track:
-        ...
