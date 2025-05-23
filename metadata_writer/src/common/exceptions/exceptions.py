829d3c054722b0f68c9037ceeae8ec21ab388efd
s1tkeyz <timstudiocode@gmail.com>
2025-05-23 15:49:54 +0300

    добавил исключение дублирования альбома у артиста

--- metadata_writer/src/common/exceptions/exceptions.py
+++ metadata_writer/src/common/exceptions/exceptions.py
@@ -21,3 +21,8 @@ class NoSuchTrackException(DomainException):
     """ Трек отсутствует """
     def __init__(self, message: str = "Запрашиваемый трек не существует"):
         super().__init__(message, status_code=404)
+
+class OwnerAlbumDublicateException(DomainException):
+    """ У исполнителя уже есть альбом с таким именем """
+    def __init__(self, message: str = "У исполнителя уже существует альбом с таким именем"):
+        super().__init__(message, status_code=406)
