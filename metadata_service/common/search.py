77b06a49608b0fd2121453e8faf84fd14744eb4c
s1tkeyz <timstudiocode@gmail.com>
2025-05-31 21:31:27 +0300

    добавил common/search.py

---
+++ metadata_service/common/search.py
@@ -0,0 +1,58 @@
+import logging
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from typing import Literal
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+es = Elasticsearch(f"http://elasticsearch:9200")
+
+SEARCH_SIZE = 10
+
+async def search_for(what: Literal["albums", "tracks"], phrase: str, page: int):
+    """ Поиск в Elasticsearch """
+
+    response = es.search(
+        index=what,
+        body={
+            "from": (page - 1) * SEARCH_SIZE,
+            "size": SEARCH_SIZE,
+            "query": {
+                "match_phrase_prefix": {
+                    "title": phrase
+                }
+            }
+        }
+    )
+    return response["hits"]["hits"]
+
+async def add_album_to_es(album_id: int, title: str, owner_id: int, release_date: datetime):
+    """ Добавляет альбом в Elasticsearch """
+    
+    document = {
+        "title": title
+    }
+    response = es.index(index="albums", id=album_id, document=document)
+    logger.info(f"Альбом с ID {album_id} добавлен в Elasticsearch")
+
+async def rmv_album_from_es(album_id: int):
+    """ Удаляет альбом из Elasticsearch """
+
+    response = es.delete(index="albums", id=album_id)
+    logger.info(f"Альбом с ID {album_id} удален из Elasticsearch")
+
+async def add_track_to_es(track_id: int, title: str, album_id: int, explicit: bool):
+    """ Добавляет трек в Elasticsearch """
+
+    document = {
+        "title": title
+    }
+    response = es.index(index="tracks", id=track_id, document=document)
+    logger.info(f"Трек с ID {track_id} добавлен в Elasticsearch")
+
+async def rmv_track_from_es(track_id: int):
+    """ Удаляет трек из Elasticsearch """
+
+    response = es.delete(index="tracks", id=track_id)
+    logger.info(f"Альбом с ID {track_id} удален из Elasticsearch")
